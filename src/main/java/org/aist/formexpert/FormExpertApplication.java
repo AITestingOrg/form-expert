64dd2e8b654776c19280aaf4cae848773568bfc0
Justin Phillips <justin_phillips@ultimatesoftware.com>
2018-05-01 17:43:43 -0400

    Initial comment

---
+++ src/main/java/org/aist/formexpert/FormExpertApplication.java
@@ -0,0 +1,12 @@
+package org.aist.formexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormExpertApplication.class, args);
+	}
+}
