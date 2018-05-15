66987a5e09ca9920b620d83e05d4a5577af02b90
Gabriel Perez Clark <gaboperezclark@gmail.com>
2018-05-15 14:38:37 -0400

    Configured jacoco and droped coverage min to 0.8

--- src/main/java/org/aist/formexpert/controllers/AbstractorController.java
+++ src/main/java/org/aist/formexpert/controllers/AbstractorController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
 @RestController
 @RequestMapping("api/v1")
 public class AbstractorController {
