44bc43f25f3e4196baf96be483e8c51814be52f5
Justin Phillips <euangelion3o9@gmail.com>
2018-05-30 10:50:51 -0400

    Updates for running and debugging locally.

--- src/main/java/org/aist/aide/formexpert/FormExpertApplication.java
+++ src/main/java/org/aist/aide/formexpert/FormExpertApplication.java
@@ -2,8 +2,10 @@ package org.aist.aide.formexpert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FormExpertApplication {
     public static void main(String[] args) {
         SpringApplication.run(FormExpertApplication.class, args);
